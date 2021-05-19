0a28ec4402fc89326891bead6ac2dea4f2fb82e9
Kwanghoon Choi <lazyswamp@gmail.com>
2021-05-19 21:03:43 +0100

    Revised to use yapb-0.1.2 as a local library

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # arith
+
+## Build
+ - You use a local libray yapb-0.1.2.
+ - To do this, you should clone yapb into a sibling direction.
+
+## Build for commit 4b9eabe, 
+
+ - You use a Hackage libray yapb-0.1.1. To do this, you need to change package.yaml and stack.yaml.
+ - You should delete yapb >= 0.1.1 in the dependencies in package.yaml.
+ - You should add yapb >= 0.1.1 in the arith-exe of executables in package.yaml.
+ - You should delete extra-deps and ../yapb in stack.yaml.
+
--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import CommonParserUtil
 
+import TokenInterface
 import Token
 import Expr
 import Lexer
@@ -10,6 +11,7 @@ import Parser
 import EmacsServer
 import SynCompInterface
 import Control.Exception
+import Data.Typeable
 
 import System.IO
 
@@ -17,10 +19,33 @@ main :: IO ()
 main = do
   emacsServer computeCand
 
-computeCand :: String -> Bool -> IO [EmacsDataItem]
-computeCand programTextUptoCursor isSimpleMode = ((do
-  terminalList <- lexing lexerSpec programTextUptoCursor 
-  ast <- parsing parserSpec terminalList 
-  successfullyParsed)
-  `catch` \e -> case e :: LexError of _ -> handleLexError)
-  `catch` \e -> case e :: ParseError Token AST of _ -> handleParseError isSimpleMode e
+-- Todo: The following part should be moved to the library.
+--       Arguments: lexerSpec, parserSpec
+--                  isSimpleMode
+--                  programTextUptoCursor, programTextAfterCursor
+
+-- | computeCand
+data ParseErrorWithLineCol token ast = ParseErrorWithLineCol Int Int (ParseError token ast)
+  deriving (Typeable, Show)
+
+instance (TokenInterface token, Typeable token, Show token, Typeable ast, Show ast)
+  => Exception (ParseErrorWithLineCol token ast)
+
+computeCand :: String -> String -> Bool -> IO [EmacsDataItem]
+computeCand programTextUptoCursor programTextAfterCursor isSimpleMode = do
+  ((computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor
+    `catch` \e -> case e :: LexError of _ -> handleLexError)
+    `catch` \e -> case e :: ParseErrorWithLineCol Token AST of ParseErrorWithLineCol line column e -> do {
+        (_, _, terminalListAfterCursor) <- lexing_ lexerSpec line column programTextAfterCursor;
+        handleParseError isSimpleMode terminalListAfterCursor e
+        })
+    
+computeCand_ :: Bool -> String -> String -> IO [EmacsDataItem]
+computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor = do
+  (line, column, terminalListUptoCursor)  <- lexing_ lexerSpec 1 1 programTextUptoCursor
+  
+  ast <-
+    (parsing parserSpec terminalListUptoCursor
+      `catch` \e -> case e :: ParseError Token AST of  _ -> throw (ParseErrorWithLineCol line column e))
+
+  successfullyParsed
