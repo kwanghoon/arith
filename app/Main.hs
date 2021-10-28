5a84e24d6f79e9c901a052974569628e01e16dbc
Kwanghoon Choi <lazyswamp@gmail.com>
2021-10-28 14:11:06 +0100

    upgrade yapb version

--- app/Main.hs
+++ app/Main.hs
@@ -17,12 +17,14 @@ import System.IO
 
 main :: IO ()
 main = do
-  emacsServer computeCand
+  emacsServer (computeCand False)
 
 -- Todo: The following part should be moved to the library.
 --       Arguments: lexerSpec, parserSpec
 --                  isSimpleMode
 --                  programTextUptoCursor, programTextAfterCursor
+
+maxLevel = 10000
 
 -- | computeCand
 data ParseErrorWithLineCol token ast = ParseErrorWithLineCol Int Int (ParseError token ast)
@@ -31,21 +33,27 @@ data ParseErrorWithLineCol token ast = ParseErrorWithLineCol Int Int (ParseError
 instance (TokenInterface token, Typeable token, Show token, Typeable ast, Show ast)
   => Exception (ParseErrorWithLineCol token ast)
 
-computeCand :: String -> String -> Bool -> IO [EmacsDataItem]
-computeCand programTextUptoCursor programTextAfterCursor isSimpleMode = do
+computeCand :: Bool -> String -> String -> Bool -> IO [EmacsDataItem]
+computeCand debug programTextUptoCursor programTextAfterCursor isSimpleMode = do
   ((computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor
     `catch` \e -> case e :: LexError of _ -> handleLexError)
     `catch` \e -> case e :: ParseErrorWithLineCol Token AST of ParseErrorWithLineCol line column e -> do {
-        (_, _, terminalListAfterCursor) <- lexing_ lexerSpec line column programTextAfterCursor;
-        handleParseError isSimpleMode terminalListAfterCursor e
+        (_, _, terminalListAfterCursor) <- lexingWithLineColumn lexerSpec line column programTextAfterCursor;
+        handleParseError (
+          HandleParseError {
+              debugFlag=debug,
+              searchMaxLevel=maxLevel,
+              simpleOrNested=isSimpleMode,
+              postTerminalList=terminalListAfterCursor,
+              nonterminalToStringMaybe=Nothing}) e
         })
     
 computeCand_ :: Bool -> String -> String -> IO [EmacsDataItem]
 computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor = do
-  (line, column, terminalListUptoCursor)  <- lexing_ lexerSpec 1 1 programTextUptoCursor
+  (line, column, terminalListUptoCursor)  <- lexingWithLineColumn lexerSpec 1 1 programTextUptoCursor
   
   ast <-
-    (parsing parserSpec terminalListUptoCursor
+    (parsing False parserSpec terminalListUptoCursor
       `catch` \e -> case e :: ParseError Token AST of  _ -> throw (ParseErrorWithLineCol line column e))
 
   successfullyParsed
