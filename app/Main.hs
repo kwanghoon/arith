2c008287902df38ea7429f61990d05a8558eefe9
Kwanghoon Choi <lazyswamp@gmail.com>
2022-04-17 15:51:03 +0900

    test2.arith; test/Spec.hs; app/SyntaxComplection.hs

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,7 @@ import Token
 import Expr
 import Lexer
 import Parser
+import SyntaxCompletion
 
 import EmacsServer
 import SynCompInterface
@@ -25,60 +26,3 @@ main = do
 --                  isSimpleMode
 --                  programTextUptoCursor, programTextAfterCursor
 
-maxLevel = 10000
-
--- | computeCand
-data ParseErrorWithLineCol token ast = ParseErrorWithLineCol Int Int (ParseError token ast ())
-  deriving (Typeable, Show)
-
-instance (TokenInterface token, Typeable token, Show token, Typeable ast, Show ast)
-  => Exception (ParseErrorWithLineCol token ast)
-
-computeCand :: Bool -> String -> String -> Bool -> IO [EmacsDataItem]
-computeCand debug programTextUptoCursor programTextAfterCursor isSimpleMode =
-  (
-   (
-    (do ast <- parsing True
-                 parserSpec ((), 1, 1, programTextUptoCursor)
-                   (aLexer lexerSpec) (fromToken (endOfToken lexerSpec))
-        successfullyParsed)
-
---      computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor
-
-     `catch` \e -> case e :: LexError of _ -> handleLexError)
-
-     `catch` \e ->
-         case e :: ParseError Token AST () of
-           _ ->            
-
-         -- case e :: ParseErrorWithLineCol Token AST of
-         --   ParseErrorWithLineCol line column e ->
-
-             do let (_,line,column,programTextAfterCursor) = lpStateFrom e
-                compCandidates <- chooseCompCandidatesFn
-
-                handleParseError
-                  compCandidates
-                  (defaultHandleParseError lexerSpec parserSpec) {
-                      debugFlag=debug,
-                      searchMaxLevel=maxLevel,
-                      simpleOrNested=isSimpleMode,
-                      postTerminalList=[],    -- terminalListAfterCursor is set to []!
-                      nonterminalToStringMaybe=Nothing
-                      } e
-   )
-    
--- computeCand_ :: Bool -> String -> String -> IO [EmacsDataItem]
--- computeCand_ isSimpleMode programTextUptoCursor programTextAfterCursor = do
---   ast <-
---     (parsing True parserSpec ((), 1, 1, programTextUptoCursor)
---       `catch` \e ->
---         case e :: ParseError Token AST () of
---           _ ->
---             do let (_,line,column,programTextAfterCursor) = lpStateFrom e
---                putStrLn (show line)
---                putStrLn (show column)
-
---                throw (ParseErrorWithLineCol line column e))
-
---   successfullyParsed
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,56 @@
+module Main where
+
+import SyntaxCompletion (computeCand)
+import SynCompInterface
+import Test.Hspec
+
+import Config
+
+import Data.Maybe (isJust)
+import System.IO (readFile)
+
+spec = hspec $ do
+  describe "Arith expression" $ do
+    
+    let config_simple = True
+    
+    let config =
+          Configuration
+            {
+              config_SIMPLE       = config_simple,
+              config_R_LEVEL      = 1,
+              config_GS_LEVEL     = 3,
+              config_DEBUG        = False,
+              config_DISPLAY      = False,
+              config_PRESENTATION = 0,
+              config_ALGORITHM    = 3
+            }
+    
+    let benchmark1 = "./app/example/test1.arith"
+    
+    it ("[Benchmark1] ") $
+      do item benchmark1 config 
+
+    let benchmark2 = "./app/example/test2.arith"
+    
+    it ("[Benchmark2] ") $
+      do item benchmark2 config 
+
+item benchmark_file init_config  = 
+      do let test_config = init_config
+         putStrLn (show test_config)
+         
+         configMaybe <- readConfig
+         benchmark <- readFile benchmark_file
+         case configMaybe of
+           Just config ->
+             do writeConfig test_config  -- set
+                results <- computeCand False benchmark "" (config_SIMPLE test_config)
+                writeConfig init_config       -- restore
+                
+           Nothing -> isJust configMaybe `shouldBe` True
+
+  
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = spec
+  
